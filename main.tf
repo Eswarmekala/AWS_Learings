a82a75d8ad66cd40c7871195791c8e05217a4555
Eswarmekala <152360789+Eswarmekala@users.noreply.github.com>
2024-08-04 16:23:30 +0530

    ec2 instance using varible

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
-##AWS creation with defult vpc############
 provider "aws" {
     region = "us-east-1"
+  
 }
-resource "aws_instance" "example" {
-  ami = "ami-04a81a99f5ec58529"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0062f2626c2fec5d4"
-  key_name = "Test_machine"
-  associate_public_ip_address = "true"
 
-
+resource "aws_instance" "day2" {
+    ami = var.ami_value
+    instance_type = var.instance_type_value
+    subnet_id = var.subnet_id
+    associate_public_ip_address = var.associate_public_ip_address
+    key_name = var.key_name_value
+  
 }
---
+++ varibles.tf
@@ -0,0 +1,23 @@
+variable "ami_value" {
+    description = "proving the ubuntu based image"
+     
+  
+}
+
+variable "instance_type_value" {
+    description = "using free instance"
+  
+}
+
+variable "subnet_id" {
+    description = "providing the public subnet"
+  
+}
+variable "associate_public_ip_address" {
+    description = "enableing pulic ip address"
+  
+}
+variable "key_name_value" {
+    description = "using key in personal laptop"
+  
+}
